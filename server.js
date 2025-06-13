74f93a3d29374ac765dbcbc4cec7eea85b80902b
syoon117 <syoon117@gmail.com>
2025-06-12 22:16:46 -0700

    update  server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { enforcePersonaAttribution } = require('./middleware/fsvl');
-const cors = require('./middleware/cors');
 const app = express();
 
 app.use(cors());
